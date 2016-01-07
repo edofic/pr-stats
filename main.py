436c6126fb49942535e28764fe69e5715f64e19a
Andraz Bajt <andraz@bajt.me>
2016-01-07 11:23:16 +0100

    Refactor into a library with AWS Lambda support

--- main.py
+++ main.py
@@ -1,117 +1,33 @@
-import collections
+import time
 import json
 import os
+import prst
 import requests
 import sys
 
 
-base = "https://api.github.com"
-
-gh = requests.Session()
-gh.auth = (os.environ["GITHUB_USERNAME"], os.environ["GITHUB_PASSWORD"])
-
-repo_name, sprint_start, sprint_end = sys.argv[1:]
-
-
-def load_all(url):
-  params = {
-      "page": 1,
-      "per_page": 100,
-      "sort": "updated",
-      "direction": "desc",
-      "state": "all",
-  }
-  while True:
-    r = gh.get(url, params=params)
-    body = r.json()
-    if len(body) > 0:
-      for pr in body:
-        yield pr
-      params["page"] += 1
-    else:
-      break
+try:
+  import prst_config
+except:
+  class Config(object):
+    pass
+  prst_config = Config()
 
 
-def load_prs():
-  return load_all("%s/repos/%s/pulls" % (base, repo_name))
+# for running on AWS Lambda
+def lambda_handler(event, context):
+  stats = prst.PrSt(prst_config.username, prst_config.password,
+                    prst_config.repo_name,
+                    prst_config.sprint_start, prst_config.sprint_end)
+  url = getattr(prst_config, "put_url", None)
+  if url is not None:
+    url = url.format(time=int(time.time()))
+    requests.put(url, json=stats.result)
+  return json.dumps(stats.result, indent=2)
 
-
-def load_comments(pr_number):
-  return load_all("%s/repos/%s/pulls/%d/comments" %
-                  (base, repo_name, pr_number))
-
-
-def take_in_sprint(xs):
-  for x in xs:
-    updated = x.get("updated_at", x.get("created_at", None))
-    if updated > sprint_end:
-      continue
-    if updated > sprint_start:
-      yield x
-    else:
-      break
-
-
-def in_sprint(x):
-  if x is None:
-    return False
-  return x > sprint_start and x < sprint_end
-
-stats = collections.defaultdict(lambda: collections.defaultdict(int))
-opened_prs = []
-merged_prs = []
-pull_stats = collections.defaultdict(lambda: collections.defaultdict(int))
-
-
-def compute_pr_stats(ids):
-  pull_stats_total = collections.defaultdict(int)
-  computed_stats = dict()
-  for pr_num in ids:
-    pr_stats = pull_stats[pr_num]
-    computed_stats[pr_num] = pr_stats
-    for prop in ["additions", "deletions", "changed_files"]:
-      pull_stats_total[prop] += pr_stats[prop]
-  computed_stats["total"] = pull_stats_total
-  return computed_stats
-
-
-for pr in take_in_sprint(load_prs()):
-  if in_sprint(pr["created_at"]):
-    stats[pr["user"]["login"]]["opened-prs"] += 1
-    opened_prs.append(pr["number"])
-  if in_sprint(pr["merged_at"]):
-    merged_prs.append(pr["number"])
-  if in_sprint(pr["created_at"]) or in_sprint(pr["merged_at"]):
-    res = gh.get("%s/repos/%s/pulls/%d" % (base, repo_name, pr["number"]))
-    pr = res.json()
-    for prop in ["additions", "deletions", "changed_files"]:
-      pull_stats[pr["number"]][prop] = pr[prop]
-    pull_stats[pr["number"]]["opener"] = pr["user"]["login"]
-  users = set()
-  for comment in take_in_sprint(load_comments(pr["number"])):
-    login = comment["user"]["login"]
-    stats[login]["comments"] += 1
-    users.add(login)
-  for user in users:
-    stats[user]["commented-on-prs"] += 1
-
-for event in take_in_sprint(load_all("%s/repos/%s/issues/events" %
-                                     (base, repo_name))):
-  login = event["actor"]["login"]
-  stats[login][event["event"]] += 1
-  if event["event"] == "merged":
-    for prop in ["additions", "deletions", "changed_files"]:
-      stats[login]["merged_" + prop] += (
-          pull_stats[event["issue"]["number"]][prop])
-
-
-for pr_num in pull_stats:
-  for prop in ["additions", "deletions", "changed_files"]:
-    stats[login]["opened_" + prop] += pull_stats[pr_num][prop]
-
-result = {
-    "users": stats,
-    "opened PRs": compute_pr_stats(opened_prs),
-    "merged PRs": compute_pr_stats(merged_prs),
-}
-print(json.dumps(result, indent=2))
+if __name__ == "__main__":
+  prst_config.username = os.environ["GITHUB_USERNAME"]
+  prst_config.password = os.environ["GITHUB_PASSWORD"]
+  prst_config.repo_name, prst_config.sprint_start, prst_config.sprint_end = \
+      sys.argv[1:]
+  print(lambda_handler(None, None))
---
+++ prst.py
@@ -0,0 +1,117 @@
+import collections
+import requests
+
+
+base = "https://api.github.com"
+
+
+class PrSt(object):
+
+  def __init__(self, username, password, repo_name, sprint_start, sprint_end):
+    self.gh = requests.Session()
+    self.gh.auth = (username, password)
+    self.repo_name = repo_name
+    self.sprint_start = sprint_start
+    self.sprint_end = sprint_end
+
+    self.user_stats = collections.defaultdict(
+        lambda: collections.defaultdict(int))
+    self.opened_prs = []
+    self.merged_prs = []
+    self.pull_stats = collections.defaultdict(
+        lambda: collections.defaultdict(int))
+
+    self.compute()
+
+  def load_all(self, url):
+    params = {
+        "page": 1,
+        "per_page": 100,
+        "sort": "updated",
+        "direction": "desc",
+        "state": "all",
+    }
+    while True:
+      r = self.gh.get(url, params=params)
+      body = r.json()
+      if len(body) > 0:
+        for pr in body:
+          yield pr
+        params["page"] += 1
+      else:
+        break
+
+  def load_prs(self):
+    return self.load_all("%s/repos/%s/pulls" % (base, self.repo_name))
+
+  def load_comments(self, pr_number):
+    return self.load_all("%s/repos/%s/pulls/%d/comments" %
+                         (base, self.repo_name, pr_number))
+
+  def take_in_sprint(self, xs):
+    for x in xs:
+      updated = x.get("updated_at", x.get("created_at", None))
+      if updated > self.sprint_end:
+        continue
+      if updated > self.sprint_start:
+        yield x
+      else:
+        break
+
+  def in_sprint(self, x):
+    if x is None:
+      return False
+    return x > self.sprint_start and x < self.sprint_end
+
+  def compute_pr_stats(self, ids):
+    pull_stats_total = collections.defaultdict(int)
+    computed_stats = dict()
+    for pr_num in ids:
+      pr_stats = self.pull_stats[pr_num]
+      computed_stats[pr_num] = pr_stats
+      for prop in ["additions", "deletions", "changed_files"]:
+        pull_stats_total[prop] += pr_stats[prop]
+    computed_stats["total"] = pull_stats_total
+    return computed_stats
+
+  def compute(self):
+    for pr in self.take_in_sprint(self.load_prs()):
+      if self.in_sprint(pr["created_at"]):
+        self.user_stats[pr["user"]["login"]]["opened-prs"] += 1
+        self.opened_prs.append(pr["number"])
+      if self.in_sprint(pr["merged_at"]):
+        self.merged_prs.append(pr["number"])
+      if self.in_sprint(pr["created_at"]) or self.in_sprint(pr["merged_at"]):
+        res = self.gh.get("%s/repos/%s/pulls/%d" %
+                          (base, self.repo_name, pr["number"]))
+        pr = res.json()
+        for prop in ["additions", "deletions", "changed_files"]:
+          self.pull_stats[pr["number"]][prop] = pr[prop]
+        self.pull_stats[pr["number"]]["opener"] = pr["user"]["login"]
+      users = set()
+      for comment in self.take_in_sprint(self.load_comments(pr["number"])):
+        login = comment["user"]["login"]
+        self.user_stats[login]["comments"] += 1
+        users.add(login)
+      for user in users:
+        self.user_stats[user]["commented-on-prs"] += 1
+
+    for event in self.take_in_sprint(self.load_all(
+                                     "%s/repos/%s/issues/events" %
+                                     (base, self.repo_name))):
+      login = event["actor"]["login"]
+      self.user_stats[login][event["event"]] += 1
+      if event["event"] == "merged":
+        for prop in ["additions", "deletions", "changed_files"]:
+          self.user_stats[login]["merged_" + prop] += (
+              self.pull_stats[event["issue"]["number"]][prop])
+
+    for pr_num in self.pull_stats:
+      for prop in ["additions", "deletions", "changed_files"]:
+        self.user_stats[login]["opened_" + prop] += self.pull_stats[pr_num][prop]
+
+    self.result = {
+        "users": self.user_stats,
+        "opened PRs": self.compute_pr_stats(self.opened_prs),
+        "merged PRs": self.compute_pr_stats(self.merged_prs),
+    }
